d7518d953a97789d40ff48d610807f182f75b458
Mohamed Adly <95952375+Mohamedadlyi@users.noreply.github.com>
2024-06-13 16:22:35 +0000

    update model and page layout

--- src/app.py
+++ src/app.py
@@ -4,6 +4,8 @@ from dash import html, Input, Output, State, dcc
 import pandas as pd
 import joblib
 import plotly.express as px
+import plotly.graph_objects as go
+
 import re
 import nltk
 from nltk.corpus import stopwords
@@ -11,42 +13,115 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 nltk.download('stopwords')
 
-class CustomTransformer(BaseEstimator, TransformerMixin):
+# Text Preprocessing
+class TextCleaner(BaseEstimator, TransformerMixin):
     def __init__(self):
-        self.stopwords_arabic = set(stopwords.words('arabic'))
+        pass
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
-        return X.apply(self.clean_txt)
+        X = pd.Series(X)
+        return X.apply(self.clean_text)
 
-    def clean_txt(self, text):
-        text = re.sub(r'http[s]?://\S+', ' ', text)
-        text = re.sub(r'@\w+', ' ', text)
-        text = re.sub(r'\b[a-zA-Z]+\b', ' ', text)
-        text = re.sub(r'[\U00010000-\U0010ffff]', ' ', text)
-        text = re.sub(r':[a-z_]+:', ' ', text)
-        text = re.sub('[*?!#@]', ' ', text)
-        text = re.sub(r'\|\|+\\s*\d+%\s*\|\|+?[_\-\.\?]+', ' ', text)
-        text = re.sub(r'[_\-\.\"\:\;\,\'\،\♡\\\)/(\&\؟]', ' ', text)
-        text = re.sub(r'\d+', ' ', text)
-        text_tokens = text.split()
-        return ' '.join(text_tokens)
+    @staticmethod
+    def clean_text(text):
+        text = TextCleaner.remove_urls(text)
+        text = TextCleaner.remove_mentions(text)
+        text = TextCleaner.remove_english_words(text)
+        text = TextCleaner.remove_unicode_bmp(text)
+        text = TextCleaner.remove_emoji_shortcodes(text)
+        text = TextCleaner.remove_specific_punctuation(text)
+        text = TextCleaner.remove_complex_patterns(text)
+        text = TextCleaner.remove_various_punctuation(text)
+        text = TextCleaner.remove_numbers(text)
+        text = TextCleaner.remove_extra_spaces(text)
+        return text
 
-    def fit_transform(self, X, y=None):
-        self.fit(X, y)
-        return self.transform(X)
+    @staticmethod
+    def remove_urls(text):
+        return re.sub(r'http[s]?://\S+', ' ', text)
 
-def custom_joblib_loader(filepath):
-    # Import the custom transformer class so it's available during deserialization
-    import __main__
-    __main__.CustomTransformer = CustomTransformer
-    return joblib.load(filepath)
+    @staticmethod
+    def remove_mentions(text):
+        return re.sub(r'@\w+', ' ', text)
+
+    @staticmethod
+    def remove_english_words(text):
+        return re.sub(r'\b[a-zA-Z]+\b', ' ', text)
+
+    @staticmethod
+    def remove_unicode_bmp(text):
+        return re.sub(r'[\U00010000-\U0010ffff]', ' ', text)
+
+    @staticmethod
+    def remove_emoji_shortcodes(text):
+        return re.sub(r':[a-z_]+:', ' ', text)
+
+    @staticmethod
+    def remove_specific_punctuation(text):
+        return re.sub(r'[*!?#@]', ' ', text)
+
+    @staticmethod
+    def remove_complex_patterns(text):
+        return re.sub(r'\|\|+\\s*\d+%\s*\|\|+?[_\-\.\?]+', ' ', text)
+
+    @staticmethod
+    def remove_various_punctuation(text):
+        return re.sub(r'[_\-\.\"\:\;\,\'\،\♡\\\)/(\&\؟]', ' ', text)
+
+    @staticmethod
+    def remove_numbers(text):
+        return re.sub(r'\d+', ' ', text)
+
+    @staticmethod
+    def remove_extra_spaces(text):
+        return ' '.join(text.split())
+
+# Model 
+class TextClassificationModel:
+    def __init__(self, model_type='logistic'):
+        self.model_type = model_type
+        self.pipeline = None
+
+    def build_pipeline(self):
+        if self.model_type == 'logistic':
+            self.pipeline = Pipeline([
+                ('cleaner', TextCleaner()),
+                ('vectorizer', CountVectorizer()),
+                ('classifier', LogisticRegression())
+            ])
+        elif self.model_type == 'naive_bayes':
+            self.pipeline = Pipeline([
+                ('cleaner', TextCleaner()),
+                ('vectorizer', CountVectorizer()),
+                ('classifier', MultinomialNB())
+            ])
+
+    def train(self, X_train, y_train):
+        self.build_pipeline()
+        self.pipeline.fit(X_train, y_train)
+
+    def evaluate(self, X_test, y_test):
+        y_pred = self.pipeline.predict(X_test)
+        return accuracy_score(y_test, y_pred)
+
+    def save(self, filename):
+        joblib.dump(self.pipeline, filename)
+
+    def load(self, filename):
+        self.pipeline = joblib.load(filename)
+
+    def predict(self, text):
+        return self.pipeline.predict([text])
+
+
 
 # Load the model
-loaded_pipeline = custom_joblib_loader('voting_pipeline_new.pkl')
-
+nb_model = TextClassificationModel(model_type='naive_bayes')
+nb_model.load('nb_model.pkl')
+txt_cleaner =TextCleaner()
 # Initialize Dash app
 app = dash.Dash(external_stylesheets=[dbc.themes.SLATE])
 server = app.server
@@ -55,31 +130,48 @@ server = app.server
 app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
-            html.H1("تخمين اللهجات العربية",  style={'textAlign': 'center', 'marginTop': '50px','marginBottom': '20px', 'color': 'goldenrod', 'fontWeight': 'bold'})
+            html.H1("تخمين اللهجات العربية",  style={'textAlign': 'center', 'marginTop': '50px', 'marginBottom': '20px', 'color': 'gold', 'fontWeight': 'bold'})
+        ])
+    ]),
+    dbc.Row([
+        html.Div(style={"height": "20px"}), 
+        dbc.Col([
+            html.H2("(مصري - ليبي - مغربي - سوداني- لبناني)", style={'textAlign': 'center', 'marginBottom': '30px', 'color': 'gold', 'fontWeight': 'bold'})
         ])
     ]),
     dbc.Row([
         dbc.Col([
-            html.H2("(مصري - لبناني - ليبي - مغربي - سوداني)", style={'textAlign': 'center', 'marginBottom': '30px', 'color': 'goldenrod', 'fontWeight': 'bold'})
-        ])
-    ]),
-    dbc.Row([
-        dbc.Col([
-            dcc.Textarea(
+            html.Div(style={"height": "50px"}),
+            dcc.Textarea( 
                 id="text-input",
-                placeholder="Enter your text here",
-                style={"width": "100%", "height": "200px"},
-                className="mb-3"
+                placeholder="رجاء إدخال نص عربي",
+                className='dark-textarea', 
             ),
-            dbc.Button("تخمين", id="predict-button", color="light", size="lg", className="d-grid gap-2 col-6 mx-auto"),
-            html.Div(id="prediction-alert"),
-            html.Button("مسح", id="reset-button", n_clicks=0, className="d-grid gap-2 col-6 mx-auto mt-3", style={"fontSize": "20px"})
-        ], width=6),
-        dbc.Col([
-            dcc.Graph(id="probability-graph")
-        ], width=6)
+            dbc.Row([
+                html.Div(style={"height": "10px"}),   
+                dbc.Col(dbc.Button(" تخمين", id="predict-button", color="light", className=' btn-primary ms-auto', style={"width":  "190px"} )),
+                dbc.Col(html.Button("مسح", id="reset-button", n_clicks=0, className='btn btn-danger'))]),
+            dbc.Row([ 
+                html.Div(style={"height": "10px"}),   
+                dbc.Col(html.Div(id="prediction-alert"), width=20)
+                ])
+        ], width="12")
+    ]),
+    dbc.Row([dcc.Graph(id="probability-graph")] 
+        )
     ])
-])
+
+
+def empty_fig(fig= None, alert = None, text = ''):
+    fig = go.Figure()
+    fig.update_layout(
+        xaxis=dict(showgrid=False, visible=False, showticklabels=False),
+        yaxis=dict(showgrid=False, visible=False, showticklabels=False),
+        plot_bgcolor='rgba(0,0,0,0)',  # Transparent plot background
+        paper_bgcolor='rgba(0,0,0,0)'
+    )
+    return fig, alert, text
+
 
 @app.callback(
     [Output("probability-graph", "figure"),
@@ -99,34 +191,30 @@ def update_output(predict_clicks, reset_clicks, text):
     if button_id == "predict-button" and (predict_clicks is None or text is None or text.strip() == ""):
         return dash.no_update, None, text
     elif button_id == "predict-button":
-        text_series = pd.Series([text])
-        probabilities = loaded_pipeline.predict_proba(text_series)
-        target_labels = ['EG', 'LB', 'LY', 'MA', 'SD']
+        if txt_cleaner.clean_text(text) == '':
+            alert = dbc.Alert("نص غير عربي", color="cyan", className="d-grid gap-2 col-6 mx-auto", style={'textAlign': 'center', 'marginTop': '50px','marginBottom': '30px', 'color': 'black', 'fontWeight': 'bold', 'fontSize': '20px'})
+            print('me')
+            return empty_fig(alert, 'نص غير عربي , رجاء إدخال نص عربي')
+        probs = nb_model.pipeline.predict_proba(text)[0]
         target_names = ['مصري', 'لبناني', 'ليبي', 'مغربي', 'سوداني']
-        label_name_mapping = dict(zip(target_labels, target_names))
-        class_labels = loaded_pipeline.classes_
-        prob_dict = {label_name_mapping[label]: prob for label, prob in zip(class_labels, probabilities[0])}
-        final_prediction_label = loaded_pipeline.predict(text_series)[0]
-        final_prediction_name = label_name_mapping[final_prediction_label]
-        labels = list(prob_dict.keys())
-        probs = list(prob_dict.values())
-        fig = px.bar(x=labels, y=probs, labels={'x': 'الدولة', 'y': 'الاحتمال'}, title='نسبة التأكد من التخمين')
+   
+        fig = px.bar(x=target_names, y=probs, labels={'x': 'الدولة', 'y': 'الاحتمال'}, title='نسبة التأكد من التخمين')
 
-        fig.update_traces(marker_color=['goldenrod' if label == final_prediction_name else 'gray' for label in labels],
+        fig.update_traces(marker_color=['cyan' if label == target_names[probs.argmax()] else 'beige' for label in target_names],
                           text=probs, texttemplate='%{text:.2f}', textposition='outside', textfont_size=14, textfont_family='Arial', textfont_color='white')
         
         fig.update_layout(height=400, width=600, margin=dict(l=40, r=40, t=40, b=40), 
                           title={'x':0.5, 'xanchor': 'center', 'font': {'size': 20, 'family': 'Arial', 'color': 'white'}},
                           plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)',
-                          xaxis={'title': {'text': 'الدولة', 'font': {'size': 18, 'family': 'Arial', 'color': 'white'}}, 
-                                 'tickfont': {'size': 14, 'family': 'Arial', 'color': 'white'}},
-                          yaxis={'title': {'text': 'الاحتمال', 'font': {'size': 18, 'family': 'Arial', 'color': 'white'}}, 
+                          xaxis={'title': {'text': 'الدولة', 'font': {'size': 28, 'family': 'Arial', 'color': 'white'}}, 
+                                 'tickfont': {'size': 20, 'family': 'Arial', 'color': 'white'}},
+                          yaxis={'title': {'text': 'الاحتمال', 'font': {'size': 24, 'family': 'Arial', 'color': 'white'}}, 
                                  'tickfont': {'size': 14, 'family': 'Arial', 'color': 'white'}})
 
-        alert = dbc.Alert(f"التنبؤ: {final_prediction_name}", color="info", className="d-grid gap-2 col-6 mx-auto", style={'textAlign': 'center', 'marginTop': '50px','marginBottom': '30px', 'color': 'black', 'fontWeight': 'bold', 'fontSize': '20px'})
+        alert = dbc.Alert(f"التنبؤ: {target_names[probs.argmax()]}", color="cyan", className="d-grid gap-2 col-6 mx-auto", style={'textAlign': 'center', 'marginTop': '50px','marginBottom': '30px', 'color': 'black', 'fontWeight': 'bold', 'fontSize': '20px'})
         return fig, alert, text
     elif button_id == "reset-button":
-        return {}, None, ""
+        return empty_fig()
     else:
         return dash.no_update, None, text
 
